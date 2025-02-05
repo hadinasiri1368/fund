446734bde4e39401ef2cc513ca5fc5aa4e426582
h.nasiri <h.nasiri@rayanhamafza.com>
2025-02-05 17:14:33 +0330

    implement login step 4

--- src/main/java/org/fund/config/SwaggerUiConfig.java
+++ src/main/java/org/fund/config/SwaggerUiConfig.java
@@ -1,12 +1,16 @@
 package org.fund.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,10 +23,20 @@ public class SwaggerUiConfig implements WebMvcConfigurer {
     public OpenAPI customOpenAPI() {
         List<Server> servers = new ArrayList<>();
         servers.add(new Server().url(serverUrl));
-        OpenAPI openApi = new OpenAPI();
-        openApi.setServers(servers);
 
-        return openApi;
+        return new OpenAPI()
+                .servers(servers)
+                .info(new Info()
+                        .title("Financial API")
+                        .version("1.0")
+                        .description("API documentation with global tenant header"))
+                .addSecurityItem(new SecurityRequirement().addList("X-TenantId"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("X-TenantId",
+                                new SecurityScheme()
+                                        .name("X-TenantId")
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)));
     }
 
     @Override
