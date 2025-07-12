4d00191ecae5d7a48c727db8509a640895abad8f
h.nasiri <h.nasiri@rayanhamafza.com>
2025-07-12 09:13:21 +0330

    implement SequenceGenerator

--- src/main/java/org/fund/model/BaseEntity.java
+++ src/main/java/org/fund/model/BaseEntity.java
@@ -4,6 +4,7 @@ package org.fund.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.SequenceGenerator;
 import lombok.Getter;
 import lombok.Setter;
 import java.io.Serializable;
@@ -12,6 +13,11 @@ import java.util.Date;
 @Getter
 @Setter
 @MappedSuperclass
+@SequenceGenerator(
+        name = "hibernate_sequence",
+        sequenceName = "hibernate_sequence",
+        allocationSize = 1
+)
 public class BaseEntity implements Serializable {
     @Column(name = "inserted_date_time", updatable = false)
     @JsonIgnore
