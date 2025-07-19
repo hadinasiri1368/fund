e15d5f056236d3f4bba5034ce0c6d4f755757b79
p.Ahmadi <pourya.ahz@gmail.com>
2025-07-19 09:42:45 +0330

    add CustomerRequestDto, CustomerResponseDto

--- src/main/java/org/fund/baseInformation/customer/dto/CustomerResponseDto.java
+++ src/main/java/org/fund/baseInformation/customer/dto/CustomerResponseDto.java
@@ -3,23 +3,15 @@ package org.fund.baseInformation.customer.dto;
 import lombok.Getter;
 import lombok.Setter;
 import org.fund.authentication.user.dto.PersonDto;
-import org.fund.model.CustomerStatus;
-import org.fund.model.DetailLedger;
-import org.fund.validator.NotEmpty;
-import org.fund.validator.ValidateField;
 
 @Getter
 @Setter
 public class CustomerResponseDto {
     private Long id;
-    @ValidateField(fieldName = "detailLedgerId", entityClass = DetailLedger.class)
     private Long detailLedgerId;
     private String dlNumber;
-    @ValidateField(fieldName = "customerStatusId", entityClass = CustomerStatus.class)
-    @NotEmpty(fieldName = "customerStatusId")
     private Long customerStatusId;
     private String customerStatusName;
-    @ValidateField(fieldName = "customerBankAccountId", entityClass = CustomerBankAccountDto.class)
     private Long customerBankAccountId;
     private String customerBankName;
     private String accountNumber;
